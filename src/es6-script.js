5d7f155098d896200ea394fab696c0cc04b7e7c0
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-09 21:23:07 -0600

    calls test es6 script

--- src/es6-script.js
+++ src/es6-script.js
@@ -1,8 +1,3 @@
-const a = 50;
-a = 60; // shows error. You cannot change the value of const.
-const b = "Constant variable";
-b = "Assigning new value"; // shows error.
-
 // Return a promise which resolves after the specified interval
 function delay(interval) {
     return new Promise(function (resolve) {
@@ -15,5 +10,10 @@ delay(1000)
         return 5;
     })
     .then(function (value) {
-        console.log(value); // 5
+        document.getElementById('content').innerHTML = value;
     });
+
+const a = 50;
+a = 60; // shows error. You cannot change the value of const.
+const b = "Constant variable";
+b = "Assigning new value"; // shows error.
